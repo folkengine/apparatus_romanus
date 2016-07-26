765abd2fbc2f4adff08187466d9265bb4128969d
Chris <ignatz@gmail.com>
2016-07-26 15:42:56 -0400

    Removed static keyword to make it accessable to EQC.

--- src/roman.c
+++ src/roman.c
@@ -31,7 +31,7 @@ const char *sieve[] = {"M","CM","D", "CD", "C", "XC", "L", "XL", "X", "IX", "V",
 
 // Methods
 
-static int get_char_arabic_value(char roman_numeral)
+int get_char_arabic_value(char roman_numeral)
 {
     roman_numeral = toupper(roman_numeral);
     roman_numeral = roman_numeral - 'A';
@@ -41,7 +41,7 @@ static int get_char_arabic_value(char roman_numeral)
     return ROMAN_VALUES[(unsigned char) roman_numeral];
 }
 
-static int get_arabic_value(char *roman_numerals)
+int get_arabic_value(char *roman_numerals)
 {
     int sum = 0;
     size_t len = strlen(roman_numerals);
@@ -60,7 +60,7 @@ static int get_arabic_value(char *roman_numerals)
     return sum;
 }
 
-static char * get_roman_value(int arabic)
+char * get_roman_value(int arabic)
 {
     if (arabic < 1)
         return "";
